b3e4a85128726fb7ba11dbd0cbbc1117e536ee61
muhammad-azeem-confiz <muhammad.azeem@confiz.com>
2014-10-16 11:23:32 +0500

    Project is added with frontend and models

---
+++ UniversityPortal/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+
+namespace UniversityPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult FrontEnd()
+        {
+            return RedirectToAction("Index", "Home", new { area = "FrontEnd" });
+        }
+    }
+}
---
+++ UniversityPortal/Models/IdentityModels.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System;
+
+namespace UniversityPortal.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public string FirstName { get; set; }
+        public string MidleName { get; set; }
+        public string LastName { get; set; }
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+        public Nullable<System.DateTime> CreatedAt { get; set; }
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        #region Tables Creation
+
+        public virtual DbSet<ArchivedEmployee> ArchivedEmployees { get; set; }
+        public virtual DbSet<ArchivedStudent> ArchivedStudents { get; set; }
+        public virtual DbSet<AssessmentScore> AssessmentScores { get; set; }
+        public virtual DbSet<Attendence> Attendences { get; set; }
+        public virtual DbSet<Batch> Batches { get; set; }
+        public virtual DbSet<CceExamCategory> CceExamCategories { get; set; }
+        public virtual DbSet<CceGrade> CceGrades { get; set; }
+        public virtual DbSet<CceGradeSet> CceGradeSets { get; set; }
+        public virtual DbSet<CceReport> CceReports { get; set; }
+        public virtual DbSet<CceWeightage> CceWeightages { get; set; }
+        public virtual DbSet<CceWeightagesCourse> CceWeightagesCourses { get; set; }
+        public virtual DbSet<ClassDesignation> ClassDesignations { get; set; }
+        public virtual DbSet<ClassTiming> ClassTimings { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<EmployeeAdditionalDetail> EmployeeAdditionalDetails { get; set; }
+        public virtual DbSet<EmployeeAdditionalField> EmployeeAdditionalFields { get; set; }
+        public virtual DbSet<EmployeeAttendence> EmployeeAttendences { get; set; }
+        public virtual DbSet<EmployeeCategory> EmployeeCategories { get; set; }
+        public virtual DbSet<EmployeeCours> EmployeeCourses { get; set; }
+        public virtual DbSet<EmployeeDepartment> EmployeeDepartments { get; set; }
+        public virtual DbSet<EmployeeDepartmentEvent> EmployeeDepartmentEvents { get; set; }
+        public virtual DbSet<EmployeeLeave> EmployeeLeaves { get; set; }
+        public virtual DbSet<EmployeeLeaveType> EmployeeLeaveTypes { get; set; }
+        public virtual DbSet<EmployeePreviousData> EmployeePreviousDatas { get; set; }
+        public virtual DbSet<EmployeeResearch> EmployeeResearches { get; set; }
+        public virtual DbSet<EmployeesSubject> EmployeesSubjects { get; set; }
+        public virtual DbSet<Event> Events { get; set; }
+        public virtual DbSet<Exam> Exams { get; set; }
+        public virtual DbSet<ExamGroup> ExamGroups { get; set; }
+        public virtual DbSet<ExamScore> ExamScores { get; set; }
+        public virtual DbSet<FaCriteria> FaCriterias { get; set; }
+        public virtual DbSet<FaGroup> FaGroups { get; set; }
+        public virtual DbSet<FaGroupsSubject> FaGroupsSubjects { get; set; }
+        public virtual DbSet<Gardian> Gardians { get; set; }
+        public virtual DbSet<GradingLevel> GradingLevels { get; set; }
+        public virtual DbSet<News> News { get; set; }
+        public virtual DbSet<NewsComment> NewsComments { get; set; }
+        public virtual DbSet<Observation> Observations { get; set; }
+        public virtual DbSet<ObservationGroup> ObservationGroups { get; set; }
+        public virtual DbSet<RankingLevel> RankingLevels { get; set; }
+        public virtual DbSet<StudentAdditionalDetail> StudentAdditionalDetails { get; set; }
+        public virtual DbSet<StudentAdditionalField> StudentAdditionalFields { get; set; }
+        public virtual DbSet<StudentCategory> StudentCategories { get; set; }
+        public virtual DbSet<StudentPreviousData> StudentPreviousDatas { get; set; }
+        public virtual DbSet<StudentPreviousSubjectMark> StudentPreviousSubjectMarks { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<StudentSubject> StudentSubjects { get; set; }
+        public virtual DbSet<SubjectLeave> SubjectLeaves { get; set; }
+        public virtual DbSet<Subject> Subjects { get; set; }
+        public virtual DbSet<Timetable> Timetables { get; set; }
+        public virtual DbSet<TimetableEntry> TimetableEntries { get; set; }
+        public virtual DbSet<Week_Day> Week_Day { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            #region Rename identity tables
+
+            modelBuilder.Entity<ApplicationUser>()
+                .ToTable("User");
+
+            modelBuilder.Entity<IdentityUserRole>()
+                .ToTable("UserRole");
+
+            modelBuilder.Entity<IdentityUserLogin>()
+                .ToTable("UserLogin");
+
+            modelBuilder.Entity<IdentityUserClaim>()
+                .ToTable("UserClaim");
+
+            modelBuilder.Entity<IdentityRole>()
+                .ToTable("Role");
+
+            #endregion
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
