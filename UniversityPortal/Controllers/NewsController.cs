7ef604a2c2f5f3864fc77d7e655a8659699fbcb3
muhammad-azeem-confiz <muhammad.azeem@confiz.com>
2014-10-24 12:26:12 -0700

    Frontend controller working now

---
+++ UniversityPortal/Controllers/NewsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UniversityPortal.Models;
+
+namespace UniversityPortal.Controllers
+{
+    public class NewsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: /News/
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.News.ToListAsync());
+        }
+
+        // GET: /News/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = await db.News.FindAsync(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // GET: /News/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /News/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.News.Add(news);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(news);
+        }
+
+        // GET: /News/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = await db.News.FindAsync(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: /News/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(News news)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(news).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(news);
+        }
+
+        // GET: /News/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = await db.News.FindAsync(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+
+        // POST: /News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            News news = await db.News.FindAsync(id);
+            db.News.Remove(news);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
